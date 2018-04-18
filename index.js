5ecb1a8d02c71264c09562e45bca42a3e09b407d
BJR Matos <bjrmatos@gmail.com>
2018-04-18 09:42:10 -0500

    add options schema

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ var config = require('./jsreport.config.js')
 
 module.exports = function (options) {
   config.options = options
+
+  const optionsSchema = {
+    type: 'object',
+    properties: {
+      url: { type: 'string' }
+    }
+  }
+
+  config.optionsSchema = {
+    base: optionsSchema,
+    extensions: {
+      base: optionsSchema
+    }
+  }
+
   config.main = main
   config.directory = __dirname
   return config
