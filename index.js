cdf1533ed8a71e58db219ea50e9b2e9b328d49c1
BJR Matos <bjrmatos@gmail.com>
2018-04-19 13:23:53 -0500

    move optionsSchema to jsreport.config.js

--- index.js
+++ index.js
@@ -3,21 +3,6 @@ var config = require('./jsreport.config.js')
 
 module.exports = function (options) {
   config.options = options
-
-  const optionsSchema = {
-    type: 'object',
-    properties: {
-      url: { type: 'string' }
-    }
-  }
-
-  config.optionsSchema = {
-    base: optionsSchema,
-    extensions: {
-      base: optionsSchema
-    }
-  }
-
   config.main = main
   config.directory = __dirname
   return config
